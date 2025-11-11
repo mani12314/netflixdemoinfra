7caa19f3aff065c82a0d375292a8ce68c7090cd4
mani12314 <manikantapalakodeti198@gmail.com>
2025-11-11 12:28:38 +0530

    Change AWS region and instance type in main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-southeast-2"
 }
 
 resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0279a86684f669718 "
-  instance_type          = "t2.medium"
+  instance_type          = "t2.micro"
   key_name               = "jekins"
   vpc_security_group_ids = ["sg-005dac616f2223950"]
   tags = {
