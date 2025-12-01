d7c183fa0b7a36a62474406c3f2e2162c88ae892
mani12314 <manikantapalakodeti198@gmail.com>
2025-12-01 10:55:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-0b8d527345fdace59"
   instance_type          = "t3.micro"
-  key_name               = "jekins"
+  key_name               = "tom"
   vpc_security_group_ids = ["sg-005dac616f2223950"]
   tags = {
     Name = var.instance_names[count.index]
